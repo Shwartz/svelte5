f87cd453e0da99e92c4e0456e346bdc434e71cdc
andris.svarcs <andris.svarcs@yahoo.com>
2024-12-12 18:18:58 +0000

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<svelte:head>
+	<title>{$page.data.title ?? 'global title'} | end</title>
+</svelte:head>
+
+<nav>
+	<a href="/">Home</a>
+	<a href="/blog">About</a>
+</nav>
+
+<slot />
