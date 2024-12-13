328946d065189d7a5b1b5b4e3533f477b5af3671
andris.svarcs <andris.svarcs@yahoo.com>
2024-12-13 06:57:35 +0000

    Support for SCSS and structure

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,17 @@
 <script lang="ts">
 	import { page } from '$app/stores';
+	import '../app.scss';
 </script>
 
 <svelte:head>
 	<title>{$page.data.title ?? 'global title'} | end</title>
 </svelte:head>
+<div class="page">
+	<nav>
+		<a href="/">Home</a>
+		<a href="/blog">About</a>
+	</nav>
 
-<nav>
-	<a href="/">Home</a>
-	<a href="/blog">About</a>
-</nav>
+	<slot />
 
-<slot />
+</div>
