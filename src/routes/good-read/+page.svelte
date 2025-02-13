ffb692d6ded579760850ff1506d33480404fc9d6
andris.svarcs <andris.svarcs@yahoo.com>
2025-02-13 06:05:29 +0000

    Placeholder for Good Read

--- src/routes/good-read/+page.svelte
+++ src/routes/good-read/+page.svelte
@@ -2,13 +2,38 @@
 	import Header from '$lib/components/Header.svelte';
 </script>
 
-<div class="content">
-	<Header/>
-	<h1>Good Read</h1>
+<div class="goodRead">
+	<Header />
+	<h1 class="frankTitle">good read</h1>
+	<p class="intro">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+		pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+		laborum.</p>
 </div>
 
 <style lang="scss">
-  .content {
+  .goodRead {
     flex-grow: 1;
+  }
+
+  .frankTitle {
+    font-size: 9rem;
+    color: var(--textLight);
+    line-height: normal;
+  }
+
+  .intro {
+    width: 100%;
+  }
+
+  @media (min-width: 600px) {
+    .intro {
+      width: 75%;
+    }
+  }
+
+  @media (min-width: 1200px) {
+    .intro {
+      width: 50%;
+    }
   }
 </style>
